621f338c0a42294d1ead3d5218e4cead28988e62
Eric Berry <cavneb@gmail.com>
2015-10-12 14:18:25 -0600

    updated json output to use hyperlink style

--- web/views/episode_view.ex
+++ web/views/episode_view.ex
@@ -1,16 +1,28 @@
 defmodule ElixirCasts.EpisodeView do
   use ElixirCasts.Web, :view
 
-  def render("index.json", %{episodes: episodes}) do
-    %{data: render_many(episodes, ElixirCasts.EpisodeView, "episode.json")}
+  def render("index.json", %{episodes: episodes, conn: conn}) do
+    %{
+      href: episode_url(conn, :index),
+      episodes: render_many(episodes, ElixirCasts.EpisodeView, "episode_link.json", %{conn: conn})
+    }
   end
 
-  def render("show.json", %{episode: episode}) do
-    %{data: render_one(episode, ElixirCasts.EpisodeView, "episode.json")}
+  def render("show.json", %{episode: episode, conn: conn}) do
+    render_one(episode, ElixirCasts.EpisodeView, "episode.json", %{conn: conn})
   end
 
-  def render("episode.json", %{episode: episode}) do
-    %{id: episode.id,
+  def render("episode_link.json", %{episode: episode, conn: conn}) do
+    %{
+      title: episode.title,
+      pub_date: episode.pub_date,
+      href: episode_url(conn, :show, episode.id)
+    }
+  end
+
+  def render("episode.json", %{episode: episode, conn: conn}) do
+    %{href: episode_url(conn, :show, episode.id),
+      id: episode.id,
       title: episode.title,
       description: episode.description,
       link: episode.link,
