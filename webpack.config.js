8270f0d93b192132b6c603d653a1f87dbfa791a4
Eric Berry <cavneb@gmail.com>
2015-10-12 15:21:12 -0600

    removed brunch

---
+++ webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = require('./app').builder;
+
+// module.exports = {
+//   entry: "./web/static/js/app.js",
+//   output: {
+//     path: "./priv/static/js",
+//     filename: "app.js"
+//   }
+// };
